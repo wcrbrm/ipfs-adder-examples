57df459d2ab09bdada0b585ad018ed72bddd727f
wcrbrm <viclopata@gmail.com>
2019-04-04 12:02:09 +0300

    NodeFromDirectory - required force of type cast

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"log"
 
+	files "github.com/ipfs/go-ipfs-files"
 	u "github.com/ipfs/go-ipfs-util"
 	importer "github.com/ipfs/go-unixfs/importer"
 	coreiface "github.com/ipfs/interface-go-ipfs-core"
+	"github.com/ipfs/interface-go-ipfs-core/options"
 
 	chunker "github.com/ipfs/go-ipfs-chunker"
 	ipld "github.com/ipfs/go-ipld-format"
@@ -27,6 +29,24 @@ func NodeFromBufferReader(api coreiface.CoreAPI) ipld.Node {
 	return nd
 }
 
+// NodeFromDirectory example of adding virtual folder to IPFS
+func NodeFromDirectory(api coreiface.CoreAPI) ipld.Node {
+	mapFiles := map[string]files.Node{
+		"one": files.NewBytesFile([]byte("testfileA")),
+		"two": files.NewBytesFile([]byte("testfileB")),
+	}
+	dir := files.NewMapDirectory(mapFiles)
+	_ = dir.(files.Directory)
+
+	// nd, err := fileAdder.AddAllAndPin(dir)
+	k, err := api.Unixfs().Add(context.Background(), dir.(files.Directory), options.Unixfs.Wrap(false))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("k=", k.String())
+	return nil
+}
+
 func main() {
 
 	coreAPI, err := MakeAPISwarm(context.Background(), false)
@@ -34,8 +54,10 @@ func main() {
 		log.Fatal("Error is ", err)
 	}
 
-	nd := NodeFromBufferReader(coreAPI)
-	fmt.Println(" NODE=", nd.Cid().String())
-	fmt.Println("  DBG=", GetNodeDataString(coreAPI.Dag(), nd))
-	fmt.Println("  RAW=", hex.EncodeToString(nd.RawData()))
+	nd := NodeFromDirectory(coreAPI)
+	if nd != nil {
+		fmt.Println(" NODE=", nd.Cid().String())
+		fmt.Println("  DBG=", GetNodeDataString(coreAPI.Dag(), nd))
+		fmt.Println("  RAW=", hex.EncodeToString(nd.RawData()))
+	}
 }
